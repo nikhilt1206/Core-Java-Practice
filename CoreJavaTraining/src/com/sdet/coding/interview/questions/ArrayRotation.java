3be7f450f45e049e125ef5b5ceb88ae4f1344dd1
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-02-27 09:24:22 +0530

    Daily commit

---
+++ CoreJavaTraining/src/com/sdet/coding/interview/questions/ArrayRotation.java
@@ -0,0 +1,32 @@
+package com.sdet.coding.interview.questions;
+
+import java.util.Arrays;
+
+public class ArrayRotation {
+
+	public static void main(String[] args) {
+
+		int[] a = { 1, 2, 3, 4, 5 };
+		int k = 2;
+		k = k % a.length;
+		a= reverse(a, 0, k - 1); //reverse first k elements
+		//reverse remaining elements of the array
+		a=reverse(a,k,a.length-1);
+		//reverse whole array
+		a=reverse(a,0,a.length-1);
+		System.out.println(Arrays.toString(a));
+	}
+
+	private static int[] reverse(int[] a, int left, int right) {
+		int temp;
+		while (left < right) {
+			temp = a[left];
+			a[left] = a[right];
+			a[right] = temp;
+			left++;
+			right--;
+		}
+		return a;
+	}
+
+}

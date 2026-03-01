f4d8dac829f8b75f98733b34cac311a548575657
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-01 16:57:56 +0530

    Updated

--- CoreJavaTraining/src/com/sdet/coding/interview/questions/ReverseWordsOfStringUsingStringBuilder.java
+++ CoreJavaTraining/src/com/sdet/coding/interview/questions/ReverseWordsOfStringUsingStringBuilder.java
@@ -9,18 +9,9 @@ public class ReverseWordsOfStringUsingStringBuilder {
 		StringBuilder result = new StringBuilder();
 		
 		for(String word:words) {
-			char[] character=word.toCharArray();
-			int left=0;
-			int right=character.length-1;
-			while(left<right) {
-				char temp = character[left];
-				character[left]=character[right];
-				character[right]=temp;
-				left++;
-				right--;
-			}
-			result.append(character).append(" ");
-		}
+			StringBuilder reversedWord = new StringBuilder(word);
+			result.append(reversedWord.reverse().append(" "));
+			} 
 		System.out.println("Input string: " + input);
 		System.out.println("Reversed string: " + result.toString().trim());
 

038fb701294c5e79152e25dc0fedd0003dbce199
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-07 09:24:09 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/sdet/coding/interview/questions/EncodingString.java
@@ -0,0 +1,26 @@
+package com.sdet.coding.interview.questions;
+
+public class EncodingString {
+
+	public static void main(String[] args) {
+
+		String input = "ddmmmmttaaaa";
+		
+		StringBuilder sb = new StringBuilder();
+		int count =1;
+		for(int i=1;i<input.length();i++) {
+			if(input.charAt(i)==input.charAt(i-1)) {
+				count++;
+			}
+			else {
+				sb.append(input.charAt(i-1));
+				sb.append(count);
+				count=1;
+			}
+		}
+		sb.append(input.charAt(input.length()-1));
+		sb.append(count);
+		System.out.println("Encoded String: "+sb);
+	}
+
+}

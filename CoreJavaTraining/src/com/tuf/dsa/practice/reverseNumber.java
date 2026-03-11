03c145537440a9ff7d20fc573ac2f02c2d5e376f
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-11 18:56:21 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/reverseNumber.java
@@ -0,0 +1,19 @@
+package com.tuf.dsa.practice;
+
+public class reverseNumber {
+
+	public static void main(String[] args) {
+		
+		int num=5678;
+		int reversedNum=0;
+		while(num>0) {
+			int lastDigit = num%10;
+			reversedNum = (reversedNum*10)+lastDigit;
+			num=num/10;
+		}
+		
+		System.out.println("Reversed Number is: "+reversedNum);
+		
+	}
+
+}

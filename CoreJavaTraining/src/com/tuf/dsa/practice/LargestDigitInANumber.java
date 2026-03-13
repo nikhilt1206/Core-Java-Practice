06406f8d870bfd3b5d7729b673e59f278b7cb89c
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-13 08:52:06 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/LargestDigitInANumber.java
@@ -0,0 +1,21 @@
+package com.tuf.dsa.practice;
+
+public class LargestDigitInANumber {
+
+	public static void main(String[] args) {
+
+		int num =56921;
+		int largestDigit=0;
+		
+		while(num>0) {
+			int lastDigit = num%10;
+			if(lastDigit>largestDigit) {
+				largestDigit=lastDigit;
+			}
+			num=num/10;
+		}
+		System.out.println("Largest digit in a given number is: "+ largestDigit);
+
+	}
+
+}

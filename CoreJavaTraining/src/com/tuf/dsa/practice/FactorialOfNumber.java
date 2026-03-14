5e60b61dde8cecbd4c94049c13b354f33a796300
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-14 12:22:32 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/FactorialOfNumber.java
@@ -0,0 +1,18 @@
+package com.tuf.dsa.practice;
+
+public class FactorialOfNumber {
+
+	public static void main(String[] args) {
+
+		int num = 6;
+		//Factorial of 0! is always 1
+		
+		int factorial = 1;
+		for(int i=1;i<=num;i++) {
+			factorial=factorial*i;
+		}
+		System.out.println("Factorial of a given number is: "+factorial);
+
+	}
+
+}

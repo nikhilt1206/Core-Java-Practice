3ef2371cee5ab5a5a589a97e1d918d2d28632b49
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-18 20:13:57 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/PrimeNumberTillN.java
@@ -0,0 +1,35 @@
+package com.tuf.dsa.practice;
+
+public class PrimeNumberTillN {
+
+	public boolean isPrime(int num) {
+		int count =0;
+		for(int i=1;i<=num;i++) {
+			if(num%i==0) {
+				count++;
+			}
+		}
+		if(count==2) {
+			return true;
+		}
+		return false;
+		
+	}
+	public  int primeUpToN(int n) {
+		int c=0;
+		for(int i=1;i<=n;i++) {
+			if(isPrime(i)) {
+				c++;
+			}
+		}
+		return c;
+	}
+	public static void main(String[] args) {
+
+		int n=5;
+		PrimeNumberTillN pn = new PrimeNumberTillN();
+		int result = pn.primeUpToN(n);
+		System.out.println("The count of primes till "+ n + " is: "+result);
+	}
+
+}

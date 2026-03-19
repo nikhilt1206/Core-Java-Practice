bcdc4ff4e69592f6de02267aff72f89f6152db0d
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-19 11:50:05 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/GCD.java
@@ -0,0 +1,23 @@
+package com.tuf.dsa.practice;
+
+public class GCD {
+
+	public int greatestCommonDivisor(int num1, int num2) {
+		int gcd=1;
+		for(int i=2;i<=Math.min(num1, num2);i++) {
+			if(num1%i==0 && num2%i==0) {
+				gcd=i;
+			}
+		}
+		return gcd;
+	}
+	public static void main(String[] args) {
+
+		int n1=6,n2=12;
+		GCD res = new GCD();
+		int gcdOfTwoNumber = res.greatestCommonDivisor(n1, n2);
+		System.out.println("GCD of "+n1+" and "+n2+ " is : "+gcdOfTwoNumber);
+
+	}
+
+}

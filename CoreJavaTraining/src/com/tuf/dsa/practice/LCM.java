439e8d1081e5732570ada3ea6ad7ec3a6edd261f
Nikhil Tiwari <nikhilt1206@gmail.com>
2026-03-20 15:39:36 +0530

    Daily Commit

---
+++ CoreJavaTraining/src/com/tuf/dsa/practice/LCM.java
@@ -0,0 +1,28 @@
+package com.tuf.dsa.practice;
+
+public class LCM {
+
+	public int LCMOfTwoNumbers(int n1, int n2) {
+		int lcm;
+		int n = Math.max(n1, n2);
+		int i=1;
+		while(true) {
+			int mul = n*i;
+			if(mul%n1==0 && mul%n2==0) {
+				lcm=mul;
+				break;
+			}
+			i++;
+		}
+		return lcm;
+		
+	}
+	public static void main(String[] args) {
+
+		int num1 = 4,num2 = 6;
+		LCM lcm = new LCM();
+		int result = lcm.LCMOfTwoNumbers(num1, num2);
+		System.out.println("LCM of "+num1+" and "+num2+" is:"+result);
+	}
+
+}
